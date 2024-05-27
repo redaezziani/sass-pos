7fefb3b594759b31776f3503e2a2783405ca7d71
redaezziani <2003100500167@ofppt-edu.ma>
2024-05-27 18:00:06 +0000

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Saas Pos System
+    </title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class=" w-full relative min-h-screen bg-neutral-100 dark:bg-neutral-900 dark:text-neutral-50 font-sans antialiased justify-center items-center">
+    <h1>
+        Hello World
+    </h1>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
